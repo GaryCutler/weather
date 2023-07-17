6941541fca989ed5fa41390b255b41041ce3b124
GaryCutler <134020980+GaryCutler@users.noreply.github.com>
2023-07-16 21:14:48 -0400

    city name

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var cityInput = document.getElementById("weather-input");
+var startWeather = document.getElementById("start-weather")
+startWeather.addEventListener("click", testFunction)
+// var weather = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=eec895795d5d972c767275a1537c9915`;
+// fetch(weather)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+  
+function testFunction(){
+    let cityName = cityInput.value
+    let weather = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=eec895795d5d972c767275a1537c9915`;
+    fetch(weather)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    const cityNameEl = document.createElement("h2");
+    cityNameEl.textContent = data.city.name
+    document.body.appendChild(cityNameEl)
+  });
+}
+
