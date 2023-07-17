ecd30c0da67923ac3492a3523518e2dbbba23f82
GaryCutler <134020980+GaryCutler@users.noreply.github.com>
2023-07-16 22:37:14 -0400

    next commit

--- assets/script.js
+++ assets/script.js
@@ -12,18 +12,18 @@ startWeather.addEventListener("click", testFunction)
   
 function testFunction(){
     let cityName = cityInput.value
-    let weather = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=eec895795d5d972c767275a1537c9915`;
+    let weather = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=eec895795d5d972c767275a1537c9915&units=imperial`;
     fetch(weather)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
-    const cityNameEl = document.createElement("h2");
+    const cityNameEl = document.createElement("h2")
     cityNameEl.textContent = data.city.name
     document.body.appendChild(cityNameEl)
-   const weather1 = document.createElement("h2")
-   weather1.textContent = data.list[0].main.feels_like
+    const weather1 = document.createElement("h2")
+   weather1.textContent = data.list[0].main.temp
    document.body.appendChild(weather1)
 
   });
