9dae7a0802c500522b1b974716a50da8b60b6b63
GaryCutler <134020980+GaryCutler@users.noreply.github.com>
2023-07-16 23:34:21 -0400

    multiple days

--- assets/script.js
+++ assets/script.js
@@ -19,12 +19,40 @@ function testFunction(){
   })
   .then(function (data) {
     console.log(data);
+    
+    const weatherTxt1 = document.createElement("h2")
+    const weatherTxt2 = document.createElement("h2")
+    const weatherTxt3 = document.createElement("h2")
+    const weatherTxt4 = document.createElement("h2")
+    const weatherTxt5 = document.createElement("h2")
     const cityNameEl = document.createElement("h2")
     cityNameEl.textContent = data.city.name
     document.body.appendChild(cityNameEl)
     const weather1 = document.createElement("h2")
+    const weather2 = document.createElement("h2")
+    const weather3 = document.createElement("h2")
+    const weather4 = document.createElement("h2")
+    const weather5 = document.createElement("h2")
+    weatherTxt1.textContent = "weather day 1"
+    weatherTxt2.textContent = "weather day 2"
+    weatherTxt3.textContent = "weather day 3"
+    weatherTxt4.textContent = "weather day 4"
+    weatherTxt5.textContent = "weather day 5"
    weather1.textContent = data.list[0].main.temp
+   weather2.textContent = data.list[1].main.temp
+   weather3.textContent = data.list[2].main.temp
+   weather4.textContent = data.list[3].main.temp
+   weather5.textContent = data.list[4].main.temp
+   document.body.appendChild(weatherTxt1)
    document.body.appendChild(weather1)
+   document.body.appendChild(weatherTxt2)
+   document.body.appendChild(weather2)
+   document.body.appendChild(weatherTxt3)
+   document.body.appendChild(weather3)
+   document.body.appendChild(weatherTxt4)
+   document.body.appendChild(weather4)
+   document.body.appendChild(weatherTxt5)
+   document.body.appendChild(weather5)
 
   });
 }
